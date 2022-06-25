1e23a08e467fae195a3208258a8602f7bc70e1ba
Olamide Okunola <olamideokunola@yahoo.com>
2022-06-25 18:19:48 +0100

    https addedd to apollo

--- src/services/dataaccessors/apollo.js
+++ src/services/dataaccessors/apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
 
-const APIURL = `http://${process.env.REACT_APP_INDEXING_SERVER_HOST}:8000/subgraphs/name/cryptobankdev/paymentgraph`;
+const APIURL = `https://${process.env.REACT_APP_INDEXING_SERVER_HOST}/subgraphs/name/cryptobankdev/paymentgraph`;
 
 const client = new ApolloClient({  
     uri: APIURL,  
